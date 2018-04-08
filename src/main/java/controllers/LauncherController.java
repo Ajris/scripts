d93c1d892006ece7c9c6e829ad11e1ad34f4f5b9
Hubi <wislak98@gmail.com>
2018-04-08 18:31:26 +0200

    added upload button and saving file in directory /home/ajris/scripts

--- src/main/java/controllers/LauncherController.java
+++ src/main/java/controllers/LauncherController.java
@@ -4,6 +4,11 @@ import services.launcher.LauncherService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
 
 @RestController
 public class LauncherController {
@@ -19,5 +24,15 @@ public class LauncherController {
     @GetMapping(value = "/launcherDownloader", produces = SHELL_SCRIPT)
     public void getLauncherForScripts(HttpServletRequest request, HttpServletResponse response) {
         launcherService.prepareLauncherAndStartDownload(request,response);
+    }
+
+    @PostMapping(value = "/launcherUploader")
+    public void uploadLauncher(HttpServletRequest request) throws FileNotFoundException{
+        String title = request.getParameter("scriptTitle").trim();
+        String text = request.getParameter("scriptText").trim();
+        File file = new File("/home/ajris/scripts/" + title);
+        PrintWriter writer = new PrintWriter(file);
+        writer.println(text);
+        writer.close();
     }
 }
