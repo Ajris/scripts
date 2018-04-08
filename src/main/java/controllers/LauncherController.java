9a460012d15b5c1e03a5c21f056e55f2ecf26d7d
Hubi <wislak98@gmail.com>
2018-04-08 19:14:02 +0200

    uploading file is working now. files are saved in /home/ajris/scripts/

--- src/main/java/controllers/LauncherController.java
+++ src/main/java/controllers/LauncherController.java
@@ -1,14 +1,18 @@
 package controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import services.launcher.LauncherService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Null;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
+import java.util.Optional;
 
 @RestController
 public class LauncherController {
@@ -27,12 +31,27 @@ public class LauncherController {
     }
 
     @PostMapping(value = "/launcherUploader")
-    public void uploadLauncher(HttpServletRequest request) throws FileNotFoundException{
-        String title = request.getParameter("scriptTitle").trim();
-        String text = request.getParameter("scriptText").trim();
+    public ResponseEntity<File> uploadLauncher(HttpServletRequest request){
+
+        String title = request.getParameter("scriptTitle");
+        String text = request.getParameter("scriptText");
+
+        if(title == null)
+            title = "";
+        title = title.trim();
+        if(text != null)
+            text = text.trim();
+
         File file = new File("/home/ajris/scripts/" + title);
-        PrintWriter writer = new PrintWriter(file);
-        writer.println(text);
-        writer.close();
+
+        try{
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(text);
+            writer.close();
+            return new ResponseEntity<>(file, HttpStatus.OK);
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(file, HttpStatus.NOT_FOUND);
+        }
     }
 }
