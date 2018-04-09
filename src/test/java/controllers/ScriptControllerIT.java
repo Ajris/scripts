d81846703510b2e0adfcc4400817235ab78a1391
Hubi <wislak98@gmail.com>
2018-04-10 01:38:55 +0200

    checkIfUnusedScriptIsSaved works

--- src/test/java/controllers/ScriptControllerIT.java
+++ src/test/java/controllers/ScriptControllerIT.java
@@ -1,14 +1,33 @@
 package controllers;
 
+import entity.Script;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @EnableAutoConfiguration
 @EnableMongoRepositories(basePackages = {"services"})
 public class ScriptControllerIT {
+
+    @Mock
+    ScriptController scriptController;
+
+    @Test
+    public void checkIfUnusedScriptIsSaved(){
+        Script script = new Script("1", "1");
+        Mockito.doNothing().when(scriptController).uploadScript(script.getTitle(), script.getText());
+        scriptController.uploadScript(script.getTitle(), script.getText());
+
+        Mockito.verify(scriptController, Mockito.times(1)).uploadScript("1","1");
+
+    }
 }
