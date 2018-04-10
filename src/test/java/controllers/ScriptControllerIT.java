b3dd547a395337415dc99051256cfc1168cd466d
Hubi <wislak98@gmail.com>
2018-04-10 02:45:07 +0200

    type fix

--- src/test/java/controllers/ScriptControllerIT.java
+++ src/test/java/controllers/ScriptControllerIT.java
@@ -1,17 +1,11 @@
 package controllers;
 
-import app.Application;
 import entity.Script;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -19,9 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import services.ScriptRepository;
 
-import java.util.Optional;
-
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -43,7 +34,7 @@ public class ScriptControllerIT {
     public void checkIfNonExistingScriptSaves() {
         Script script = new Script("1", "1");
         doNothing().when(scriptController).uploadScript(script.getTitle(), script.getText());
-        if(!scriptRepository.findByTitle(script.getTitle()).isPresent()){
+        if (!scriptRepository.findByTitle(script.getTitle()).isPresent()) {
             scriptController.uploadScript(script.getTitle(), script.getText());
         }
         verify(scriptController, times(1)).uploadScript(script.getTitle(), script.getText());
@@ -52,7 +43,7 @@ public class ScriptControllerIT {
     @Test(expected = DuplicateKeyException.class)
     public void checkIfExistingScriptThrowAnException() {
         Script script = new Script("first", "A");
-        if(scriptRepository.findByTitle(script.getTitle()).isPresent()){
+        if (scriptRepository.findByTitle(script.getTitle()).isPresent()) {
             when(scriptRepository.save(script)).thenThrow(DuplicateKeyException.class);
         }
         scriptController.uploadScript(script.getTitle(), script.getText());
